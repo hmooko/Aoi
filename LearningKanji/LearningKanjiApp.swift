37ce169329af204757fb9cc0f8110a4b12c7b193
koohyunmo <hmoo4198@gmail.com>
2024-07-24 14:30:12 +0900

    Initial Commit

---
+++ LearningKanji/LearningKanjiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningKanjiApp.swift
+//  LearningKanji
+//
+//  Created by koohyunmo on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningKanjiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
